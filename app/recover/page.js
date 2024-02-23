e3ff96736271b8dca22585cf9c131905b91b8630
nicom <nicomontejano15@gmail.com>
2024-02-24 00:48:51 +0100

    recover password

--- app/recover/page.js
+++ app/recover/page.js
@@ -1,29 +1,28 @@
 "use client"
-// pages/signin.js
+// pages/recover.js
 import { useState } from 'react';
 import Link from 'next/link'; // Importa la etiqueta <Link> de Next.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../globals.css';
 
-const SignInPage = () => {
+const RecoverPage = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await fetch('/api/login', {
+  /* try {
+      const response = await fetch('/api/sendRecoveryEmail', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email }),
       });
 
       if (response.ok) {
-        // Redirigir a la página principal u otra página después de iniciar sesión
-        window.location.href = '/';
+        // Redirigir a la página de introducir nueva contraseña
+        window.location.href = '/recover/password';
       } else {
         const data = await response.json();
         setErrorMessage(data.message);
@@ -31,15 +30,17 @@ const SignInPage = () => {
     } catch (error) {
       console.error('An unexpected error occurred:', error);
       setErrorMessage('An unexpected error occurred');
-    }
+    }*/
+    window.location.href="/recover/password"
   };
 
   return (
     <div className="container mt-5">
-      <h2>Sign In</h2>
+      <h2>¿Has olvidado tu contraseña?</h2>
+      <p className="mb-4">Indícanos cuál es tu correo electrónico y te enviaremos un enlace para que puedas recuperar tu contraseña.</p>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email:</label>
+          <label htmlFor="email" className="form-label">Correo electrónico:</label>
           <input
             type="email"
             id="email"
@@ -49,26 +50,11 @@ const SignInPage = () => {
             required
           />
         </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password:</label>
-          <input
-            type="password"
-            id="password"
-            className="form-control"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Sign In</button>
+        <button type="submit" className="btn btn-primary">Siguiente</button>
         {errorMessage && <p className="text-danger mt-2">{errorMessage}</p>}
-        <div className="mb-3">
-          {/* Cambia el elemento <a> por <span> */}
-          <p><Link href="/recover"><span>¿Has olvidado la contraseña? </span></Link></p>
-        </div>
       </form>
     </div>
   );
 };
 
-export default SignInPage;
+export default RecoverPage;
---
+++ app/recover/password/page.js
@@ -0,0 +1,75 @@
+"use client"
+// pages/recover/password.js
+import { useState } from 'react';
+import Link from 'next/link'; // Importa la etiqueta <Link> de Next.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../globals.css';
+
+const RecoverPasswordPage = () => {
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newPassword !== confirmPassword) {
+      setErrorMessage('Las contraseñas no coinciden');
+      return;
+    }
+    try {
+      const response = await fetch('/api/resetPassword', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ newPassword }),
+      });
+
+      if (response.ok) {
+        // Contraseña restablecida correctamente
+        // Puedes redirigir al usuario a otra página o mostrar un mensaje de éxito aquí
+      } else {
+        const data = await response.json();
+        setErrorMessage(data.message);
+      }
+    } catch (error) {
+      console.error('An unexpected error occurred:', error);
+      setErrorMessage('An unexpected error occurred');
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2>Recuperar contraseña</h2>
+      <p className="mb-4">Crear una nueva contraseña</p>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="newPassword" className="form-label">Nueva contraseña:</label>
+          <input
+            type="password"
+            id="newPassword"
+            className="form-control"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirmPassword" className="form-label">Repetir nueva contraseña:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            className="form-control"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Recuperar</button>
+        {errorMessage && <p className="text-danger mt-2">{errorMessage}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default RecoverPasswordPage;
