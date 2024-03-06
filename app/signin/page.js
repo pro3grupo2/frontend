240a8ee4ce9ba764b01e9fb17d2f3590908c1c23
0SC4R24 <oscarviudez24@gmail.com>
2024-03-06 23:57:17 +0100

    Codigo limpiado

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,18 +1,14 @@
 "use client"
 
 import { useState } from 'react';
-import Link from 'next/link';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from '../../components/NavBar';
-import '../styles/signin-styles.css';
-import Image from 'next/image'; // Importa la etiqueta <Image> de Next.js
+import { useRouter } from "next/navigation";
 
 const { signin } = require('../../api/sign');
 
-const SignInPage = () => {
+export default function SignIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+    const router = useRouter();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -21,54 +17,13 @@ const SignInPage = () => {
 
         if (token) {
             localStorage.setItem('token', token);
-            window.location.href = '/home';
-        } else {
-            setErrorMessage('Email o contraseña incorrectos');
+            router.push("/home");
         }
     };
 
     return (
-        <div className="container">
-            <div className="split-container">
-                <div className="form-container">
-                    <div className="text-container">
-                        <h1>Iniciar Sesión con el Correo de U-tad</h1>
-                        <p className="subtext">El desarrollo de proyectos es una carta de presentación de los conocimientos, experiencia y capacidad de trabajo en equipo.</p>
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                            <input
-                                type="email"
-                                id="email"
-                                className="form-control"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                placeholder="Correo Electrónico"
-                                required
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <input
-                                type="password"
-                                id="password"
-                                className="form-control"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Contraseña"
-                                required
-                            />
-                        </div>
-                        <p><Link href="/recover"><span>¿Has olvidado la contraseña? </span></Link></p>
-                        <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
-                        {errorMessage && <p className="text-danger mt-2">{errorMessage}</p>}
-                        <p>¿Aún no tienes cuenta? <Link href="/signup">¡Inscríbete ahora!</Link></p>
-                    </form>
-                </div>
-                <div className="image-container">
-                </div>
-            </div>
-        </div>
-    );
-};
+        <div>
 
-export default SignInPage;
+        </div>
+    )
+}
--- components/NavBar.js
+++ components/NavBar.js
@@ -2,24 +2,21 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-const NavBar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container d-flex justify-content-between align-items-center">
-        {/* Logo de U-tad a la izquierda */}
-        <div>
-          <Image src="/images/logo.png" alt="Logo de U-tad" height={100} width={180} />
-        </div>
-        {/* Información de contacto y botón de inicio de sesión a la derecha */}
-        <div className="d-flex align-items-center">
-          <p className="m-0 mr-3">900 373 379</p>
-          <Link href="/signin">
-            <button className="btn btn-primary">Sign in</button>
-          </Link>
-        </div>
-      </div>
-    </nav>
-  );
-};
-
-export default NavBar;
+export default function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid p-3 d-flex justify-content-between">
+                <Image className="img-fluid" src="/images/logo_utad.png" alt="Logo de U-tad" height={100} width={180} />
+                <div className="d-flex align-items-center">
+                    <p className="m-0 mr-3 p-3 ms-light"> 900 373 379 </p>
+                    <Link className="mx-3" href="/signin">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 42 42" fill="none">
+                            <path d="M21 1C23.5196 1 25.9359 2.00089 27.7175 3.78249C29.4991 5.56408 30.5 7.98044 30.5 10.5C30.5 13.0196 29.4991 15.4359 27.7175 17.2175C25.9359 18.9991 23.5196 20 21 20C18.4804 20 16.0641 18.9991 14.2825 17.2175C12.5009 15.4359 11.5 13.0196 11.5 10.5C11.5 7.98044 12.5009 5.56408 14.2825 3.78249C16.0641 2.00089 18.4804 1 21 1ZM21 27.25C26.6798 27.25 31.7702 28.4022 35.4037 30.219C39.0968 32.0655 41 34.432 41 36.75V41H1V36.75C1 34.432 2.90325 32.0655 6.59628 30.219C10.2298 28.4022 15.3202 27.25 21 27.25Z" stroke="black" strokeWidth="2"/>
+                        </svg>
+                    </Link>
+                    <button className="border border-0 bg-transparent">EN</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
