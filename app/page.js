8cf0a56f884b3a62c9ea388dfade7b48a01c169e
0SC4R24 <oscarviudez24@gmail.com>
2024-03-07 20:51:44 +0100

    image bg

--- app/page.js
+++ app/page.js
@@ -12,15 +12,12 @@ async function ME(token) {
 
 export default function HomePage() {
     return (
-        <div className="container-fluid text-center d-flex justify-content-center align-items-center" style={{ minHeight: '80vh' }}>
-            <div className="container">
-                {/* <Image src="/images/img_placeholder.png" className="position-absolute top-50 start-50 translate-middle" alt="" width={1080} height={720} /> */}
-                <h1 className="display-1 ms-black mb-4">Trabajos de alumnos/nis</h1>
-                <p className="ms-extrabold fs-2 mb-4">¡Descubre aquí el talento U-tad!</p>
-                <p className="ms-light fs-5 mb-0">Bienvenido al repositorio de proyectos oficial de la U-tad.</p>
-                <p className="ms-light fs-5 mb-5">Aquí podrás encontrar todo tipo de trabajos de los alumnos que se han formado aquí!</p>
-                <button className="btn btn-primary mt-5 ms-button fs-2 px-5 w-50 border-5 btn-lg py-1 text-uppercase">Acceder</button>
-            </div>
+        <div className="container-fluid bg-image-main text-center d-flex flex-column justify-content-center align-items-center" style={{minHeight: "85vh"}}>
+            <h1 className="display-1 ms-black mb-4">Trabajos de alumnos/nis</h1>
+            <p className="ms-extrabold fs-2 mb-4">¡Descubre aquí el talento U-tad!</p>
+            <p className="ms-light fs-5 mb-0">Bienvenido al repositorio de proyectos oficial de la U-tad.</p>
+            <p className="ms-light fs-5 mb-5">Aquí podrás encontrar todo tipo de trabajos de los alumnos que se han formado aquí!</p>
+            <button className="btn btn-primary mt-5 ms-button fs-2 px-5 w-50 border-5 btn-lg py-1 text-uppercase">Acceder</button>
         </div>
     )
 }
