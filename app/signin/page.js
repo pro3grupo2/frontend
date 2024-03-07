7cb541078fb3d407ebe49dcad1a9153bf1617d65
adriantoral <adrian.rubiboy11@gmail.com>
2024-03-08 00:28:40 +0100

    Actualiza el signin

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,11 +1,10 @@
 "use client";
 
-import { useState } from 'react';
-import { useRouter } from "next/navigation";
+import {useState} from 'react';
+import {useRouter} from "next/navigation";
 import Link from 'next/link';
-import Image from 'next/image';
 
-import { signin } from '@/api/v1/auth';
+import {signin} from '@/api/v1/auth';
 
 export default function SignIn() {
     const [email, setEmail] = useState('');
@@ -14,7 +13,6 @@ export default function SignIn() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(email); 
 
         const token = await signin(email, password)
         if (token) {
@@ -24,82 +22,71 @@ export default function SignIn() {
     }
 
     return (
-        <div className="container mt-5 pt-5">
-            <div className="row">
-                <div className="col-md-6">
-                    {/* Formulario */}
-                    <div className="split-container">
-                        <div className="form-container">
-                            <div className="text-container ">
-                                <h1 className='ms-black'>Iniciar Sesión con el Correo de U-tad</h1>
-                                <p className="subtext py-2 mb-4">
-                                    El desarrollo de proyectos es una carta de presentación de los
-                                    conocimientos, experiencia y capacidad de trabajo en equipo.
-                                </p>
-                            </div>
-                            <form onSubmit={handleSubmit}>
-                                <div className="py-3 ">
-                                    <input
-                                        type="email"
-                                        id="email"
-                                        className="form-control py-3"
-                                        style={{ backgroundColor: "var(--secundario-gris-claro)" }}
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        placeholder="Correo Electrónico"
-                                        required
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <input
-                                        type="password"
-                                        id="password"
-                                        className="form-control py-3"
-                                        style={{ backgroundColor: "var(--secundario-gris-claro)" }}
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        placeholder="Contraseña"
-                                        required
-                                    />
-                                </div>
-                                <p>
-                                    <Link className="link-underline-dark link-dark fw-bold" href="/recover">
-                                        <span >
+        <div className="container-fluid">
+            <div className="row vh-100">
+                <div className="form-container col-12 col-xl-4 align-self-center px-5">
+                    <div>
+                        <h1 className="display-3 ms-extrabold">Iniciar Sesión con el Correo de U-tad</h1>
+                        <p className="lead py-2 mb-4">
+                            El desarrollo de proyectos es una carta de presentación de los
+                            conocimientos, experiencia y capacidad de trabajo en equipo.
+                        </p>
+                    </div>
+
+                    <form onSubmit={handleSubmit}>
+                        <div className="py-3 ">
+                            <input
+                                type="email"
+                                id="email"
+                                className="form-control py-3"
+                                style={{backgroundColor: "var(--secundario-gris-claro)"}}
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="Correo Electrónico"
+                                required
+                                autoComplete="off"
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                type="password"
+                                id="password"
+                                className="form-control py-3"
+                                style={{backgroundColor: "var(--secundario-gris-claro)"}}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="Contraseña"
+                                required
+                                autoComplete="off"
+                            />
+                        </div>
+                        <p>
+                            <Link className="link-underline-dark link-dark fw-bold" href="/recover">
+                                        <span>
                                             ¿Has olvidado la contraseña?
                                         </span>
-                                    </Link>
-                                </p>
-                                <button
-                                    type="submit"
-                                    className="btn ms-black btn-primary mt-4 w-100 border-5 btn-lg py-3 fs-3">
-                                    INICIAR SESIÓN
-                                </button>
-                            </form>
-                            
-                            <p className="text-center">
-                                <span >
+                            </Link>
+                        </p>
+                        <button
+                            type="submit"
+                            className="btn btn-primary mt-4 w-100 border-5 btn-lg py-3 fs-3">
+                            INICIAR SESIÓN
+                        </button>
+                        <p className="text-center">
+                                <span>
                                     ¿Aún no tienes cuenta?{" "}
                                     <Link className="link-underline-dark link-dark fw-bold" href="/signup">
-                                        <span >¡Inscríbete ahora!</span>
+                                        <span>¡Inscríbete ahora!</span>
                                     </Link>
                                 </span>
-                            </p>
-                        </div>
-                    </div>
+                        </p>
+                    </form>
                 </div>
-                <div className="col-md-6">
-                    <div className="image-container ">
-                        <Image
-                            src="/images/img_placeholder.png"
-                            alt="Descripción de la imagen"
-                            width={0}
-                            height={0}
-                            sizes="(max-width: 80px) 50vw, 100vw"
-                            className="img-fluid h-100 w-100"
-                        />
-                    </div>
+
+                <div className="d-none d-xl-inline col-xl-8">
+                    <div className="bg-image-main"></div>
                 </div>
             </div>
         </div>
-    );
+    )
 }
