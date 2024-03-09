122cfb5c18a8fa210f59e4cccdefed65af3705bc
iviguio <i.guio.aunon@gmail.com>
2024-03-09 23:38:55 +0100

    SOLUCIONADO fallo seguridad home
    La pagina home cargaba todos los datos y luego verificaba. Esto es un fallo de seguridad. Ahora primero verifica el token y redirige en caso de que no valga. Si el token es valido entonces ya si muestra el contenido.

--- app/home/page.js
+++ app/home/page.js
@@ -6,28 +6,38 @@ import { me } from '@/api/v1/auth';
 
 export default function Home() {
     const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
     const router = useRouter();
 
     useEffect(() => {
         const token = localStorage.getItem('token');
         
-        if (token) {
+        if (!token) {
+            router.push('/signin');
+        } else {
             me(token).then((data) => {
                 setUser(data);
-                console.log(data);
+                setIsLoading(false); // desactivo carga de pagina web
+            }).catch(() => {
+                // si token invalido entonces redirige
+                router.push('/signin');
             });
-        } else {
-            router.push('/signin');
         }
-    }, []);
+    }, [router]);
 
-    if (user === undefined) {
-        router.push('/signin');
+    if (isLoading) {
+        return <div>Cargando...</div>; // pagina mientras verifica token
     }
 
-    return (
+    // si ha cargado pero hay algun problema con el user tambien redirige
+    if (!user) {
+        router.push('/signin');
+        return null;
+    }
+    //contenido del home
+    return ( 
         <div className="container-fluid text-center d-flex flex-column justify-content-center align-items-center" style={{ height: '100vh' }}>
             <h1 className="display-1 ms-extrabold">Bienvenido {user.rol}, {user.nombre_completo}!</h1>
         </div>
-    )
+    );
 }
