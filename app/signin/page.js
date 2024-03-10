ef6979fe226b75f14e8eb49e4af788937af8460a
adriantoral <adrian.rubiboy11@gmail.com>
2024-03-10 02:00:32 +0100

    Actualiza la pagina de signin para ajustar los espacios segun ha dicho Oscar S

--- app/signin/page.js
+++ app/signin/page.js
@@ -30,55 +30,55 @@ export default function SignIn() {
             <NavBar/>
 
             <div className="d-flex flex-row flex-grow-1 justify-content-evenly align-items-center">
-                <div className="px-5">
-                    <div className="form-container flex-shrink-1 p-5">
-                        <div>
-                            <h1 className="display-3 ms-extrabold">Iniciar sesión con el correo de la U-tad</h1>
-                            <p className="d-none d-sm-block lead">
-                                El desarrollo de proyectos es una carta de presentación de los
-                                conocimientos, experiencia y capacidad de trabajo en equipo.
-                            </p>
-                        </div>
+                <div className="flex-shrink- px-5">
+                    <div className="pb-4">
+                        <h1 className="display-3 ms-extrabold">Iniciar sesión con el correo de la U-tad</h1>
+                        <p className="d-none d-sm-block lead">
+                            El desarrollo de proyectos es una carta de presentación de los
+                            conocimientos, experiencia y capacidad de trabajo en equipo.
+                        </p>
+                    </div>
 
-                        <form className="row row-gap-3" onSubmit={handleSubmit}>
-                            <input
-                                type="email"
-                                id="email"
-                                className="form-control py-3"
-                                style={{backgroundColor: "var(--secundario-gris-claro)"}}
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                placeholder="Correo Electrónico"
-                                required
-                                autoComplete="off"
-                            />
+                    <form className="row row-gap-3 m-0" onSubmit={handleSubmit}>
+                        <input
+                            type="email"
+                            id="email"
+                            className="form-control py-3"
+                            style={{backgroundColor: "var(--secundario-gris-claro)"}}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Correo Electrónico"
+                            required
+                            autoComplete="off"
+                        />
 
-                            <input
-                                type="password"
-                                id="password"
-                                className="form-control py-3"
-                                style={{backgroundColor: "var(--secundario-gris-claro)"}}
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Contraseña"
-                                required
-                                autoComplete="off"
-                            />
+                        <input
+                            type="password"
+                            id="password"
+                            className="form-control py-3"
+                            style={{backgroundColor: "var(--secundario-gris-claro)"}}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Contraseña"
+                            required
+                            autoComplete="off"
+                        />
 
-                            <Link className="link-underline-dark link-dark fw-bold" href="/recover">¿Has olvidado la contraseña?</Link>
+                        <Link className="link-underline-dark link-dark fw-bold" href="/recover">¿Has olvidado la contraseña?</Link>
 
+                        <div className="pt-4 px-0">
                             <button
                                 type="submit"
-                                className="btn btn-primary border-5 py-3 fs-3">
+                                className="w-100 btn btn-primary border-5 py-3 fs-3">
                                 INICIAR SESIÓN
                             </button>
 
-                            <div className="text-center">
+                            <div className="col-12 text-center">
                                 <span className="pe-1">¿Aún no tienes cuenta?</span>
                                 <Link className="link-underline-dark link-dark fw-bold ps-1" href="/signup">¡Inscríbete ahora!</Link>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
 
                 <div className="d-none d-xl-block bg-image-main w-100"></div>
