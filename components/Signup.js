9792845676cd1b86773a1349bb1c78366ef447d8
iviguio <i.guio.aunon@gmail.com>
2024-03-13 11:36:02 +0100

    FIX PASO 0
    Se ha recargado un commit anterior ya que se actualizó este y perdió el diseño responsive del paso 0. NO TOCAR,  PASO 0. VERSION FINAL

--- components/Signup.js
+++ components/Signup.js
@@ -5,8 +5,6 @@ import { useState } from 'react';
 const PasoInicio = ({ setNextPaso, setPreviousPaso, setEmail }) => {
     const [email, setEmailLocal] = useState('');
     const [emailError, setEmailError] = useState(false);
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
 
     const handleEmailChange = (e) => {
         const value = e.target.value;
@@ -20,7 +18,7 @@ const PasoInicio = ({ setNextPaso, setPreviousPaso, setEmail }) => {
 
     const handleNextClick = () => {
         const isValidEmail = email.includes('@');
-        setEmailError(!isValidEmail);
+        setEmailError(!isValidEmail); 
         if (isValidEmail) {
             setNextPaso();
         }
@@ -29,16 +27,15 @@ const PasoInicio = ({ setNextPaso, setPreviousPaso, setEmail }) => {
     return (
         <>
             <div className="d-flex flex-row flex-grow-1 justify-content-evenly align-items-center">
-                <div className="flex-shrink- px-5 mx-xl-5">
-                    <div className="pb-5">
-                        <h1 className="display-5 custom-bold">Crear cuenta</h1>
-                        <p className="d-none fs-5 d-sm-block lead">
-                            Indícanos cuál es tu correo asociado a la U-tad.
-                        </p>
-                    </div>
-
-                    <form className='row row-gap m-0 mt-5 py-5'>
-                        <div className="input-grop mb-3 px-0" style={{ width: "100%" }}>
+                <div className="flex-shrink- px-5 mx-xl-5 w-100" style={{ height: '90%' }}>
+                    <div className="d-flex flex-column justify-content-between h-100">
+                        <div className="pb-4 mt-5">
+                            <h1 className="display-4 ms-extrabold">Crear cuenta</h1>
+                            <p className="d-none fs-5 d-sm-block lead">
+                                Indícanos cuál es tu correo asociado a la U-tad.
+                            </p>
+                        </div>
+                        <div>
                             <input
                                 type="email"
                                 className="form-control py-3 fs-5"
@@ -50,9 +47,9 @@ const PasoInicio = ({ setNextPaso, setPreviousPaso, setEmail }) => {
                                 autoComplete="off"
                                 value={email}
                             />
-                            <div className="d-flex justify-content-between aligns-items-center mt-5  m-0">
+                            <div className="d-flex justify-content-between mt-5">
                                 <Link href="/signin">
-                                    <button className="btn btn-outline-primary" type="button" style={{ width: '48px', height: '48px' }} >
+                                    <button className="btn btn-outline-primary" type="button" style={{width: '60px', height: '60px'}} >
                                         <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" viewBox="0 0 10 16" fill="none">
                                             <path d="M10 1.4303L8.48329 -1.48327e-06L1.39876e-06 8L8.48329 16L10 14.5697L3.03342 8L10 1.4303Z" fill="#091229" />
                                         </svg>
@@ -62,16 +59,16 @@ const PasoInicio = ({ setNextPaso, setPreviousPaso, setEmail }) => {
                                 <button
                                     type="button"
                                     onClick={handleNextClick}
-                                    className="btn btn-primary btn-color-primary btn-outline-primary border-5 py-1 px-5 fs-5 fw-bold">
-                                    SIGUIENTE
+                                    className="btn btn-primary btn-color-primary btn-outline-primary border-5 py-1 px-5 fs-3 fw-bold">
+                                    Siguiente
                                 </button>
                             </div>
                         </div>
-                    </form>
-
-                    <div className="col-12 text-center mb-5">
-                        <span className="pe-1 fs-5">¿Ya tienes una cuenta?</span>
-                        <Link className="link-underline-dark link-dark fw-bold fs-5 ps-1" href="/signin">Iniciar sesión</Link>
+                        
+                        <div className="col-12 text-center mb-5">
+                            <span className="pe-1 fs-5">¿Ya tienes una cuenta?</span>
+                            <Link className="link-underline-dark link-dark fw-bold fs-5 ps-1" href="/signin">Iniciar sesión</Link>
+                        </div>
                     </div>
                 </div>
                 <div className="d-none d-xl-block w-100" style={{ maxWidth: '5%' }}></div>
