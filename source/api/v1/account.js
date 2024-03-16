16529b9a653104bff7474aba275a3b49c83d3633
adriantoral <adrian.rubiboy11@gmail.com>
2024-03-16 23:27:59 +0100

    Agrega la posibilidad de modificar tu cuenta para el recover

---
+++ source/api/v1/account.js
@@ -0,0 +1,18 @@
+const {fetch_handler} = require('.')
+
+const ACCOUNT_ROUTE = '/account'
+
+const update = async (token, nombre_completo, password, frase_recuperacion, rol, promocion) => {
+    const headers = {
+        'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`
+    }, body = JSON.stringify({
+        nombre_completo: nombre_completo, password: password, frase_recuperacion: frase_recuperacion, rol: rol, promocion: promocion
+    })
+
+    const datos = await fetch_handler(ACCOUNT_ROUTE, headers, "UPDATE", body)
+    return datos ? datos.data : null
+}
+
+module.exports = {
+    update
+}
