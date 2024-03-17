feeda447f2ebbad1f73bb1cc4aaa8b994f0f269a
0SC4R24 <oscarviudez24@gmail.com>
2024-03-17 23:06:38 +0100

    More /project/[id]
     - Added view of images and download buttons

--- source/app/project/[id]/page.js
+++ source/app/project/[id]/page.js
@@ -4,6 +4,7 @@ import {useEffect, useState} from 'react';
 import useAuth from '@/hooks/useAuth';
 import {get_proyecto} from "@/api/v1/proyectos";
 import ProjectCard from '@/components/ProjectCard';
+import Link from 'next/link';
 
 export default function Project({params}) {
     const {user, isLoading} = useAuth();
@@ -37,30 +38,26 @@ export default function Project({params}) {
             </div>
         );
     } else {
-        console.log(proyecto);
         return (
             <div className="container py-5">
                 <p className="text-decoration-underline fs-5 ms-black">Carrera/Nombre_Proyecto/Año</p>
                 <div className="d-flex justify-content-between align-items-center">
                     <h1 className="display-1 ms-black">{proyecto.titulo}</h1>
                     <div className="d-flex justify-content-end mb-3">
-                        <button className="btn btn-outline-dark rounded-pill me-2"><span className="me-3">Editar</span> 
+                        <button className="btn btn-outline-dark rounded-pill me-2 p-3"><span className="me-4">Editar</span> 
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                                 <path d="M8.91984 0.000121768L6.91984 0.00012168L6.91984 12.0001L1.41984 6.50012L-0.000156749 7.92012L7.91984 15.8401L15.8398 7.92012L14.4198 6.50012L8.91984 12.0001L8.91984 0.000121768Z" fill="black"/>
                             </svg>
                         </button>
-                        <button className="btn btn-outline-dark rounded-pill me-2"><span className="me-3">Descargar</span> 
+                        <Link href={`https://api.reservorio-u-tad.com${proyecto.url}`} className="btn btn-outline-dark rounded-pill me-2 p-3"><span className="me-4">Descargar</span> 
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                                 <path d="M8.91984 0.000121768L6.91984 0.00012168L6.91984 12.0001L1.41984 6.50012L-0.000156749 7.92012L7.91984 15.8401L15.8398 7.92012L14.4198 6.50012L8.91984 12.0001L8.91984 0.000121768Z" fill="black"/>
                             </svg>
-                        </button>
+                        </Link>
                     </div>
                 </div>
                 <p className="fs-3 ms-extrabold mt-3 mb-4">{proyecto.usuarios.nombre_completo}</p>
-                <svg className="" width="70%" height="40%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#868e96"></rect>
-                    <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
-                </svg>
+                <img src={`https://api.reservorio-u-tad.com${proyecto.portada}`} alt="Project Image" style={{width: "100%", height: "500px"}}/>
                 <p className="fs-5 ms-regular my-4">{proyecto.ficha}</p>
                 <div className="row g-4 card-group mt-5">
                     {/* Show only 3 projects of the same user */}
@@ -74,7 +71,6 @@ export default function Project({params}) {
                     <hr className="w-50"/>
                 </div>
                 <div className="row g-4 card-group mt-5">
-                    {/* Show only 3 projects of the same user */}
                     <ProjectCard project={proyecto}/>
                     <ProjectCard project={proyecto}/>
                     <ProjectCard project={proyecto}/>
