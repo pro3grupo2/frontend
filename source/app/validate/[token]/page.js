ddcb01b1dbb946b6f91cc8048fa0a64a20180a8f
adriantoral <adrian.rubiboy11@gmail.com>
2024-03-18 22:22:00 +0100

    Agrega la animacion de Loading

--- source/app/validate/[token]/page.js
+++ source/app/validate/[token]/page.js
@@ -1,10 +1,9 @@
 "use client"
 
-import "@/styles/loading.css"
-
 import {useEffect} from 'react'
 import {useRouter} from "next/navigation"
 import {validate} from "@/api/v1/auth";
+import Loading from "@/components/Loading";
 
 export default function RecoverPassword({params}) {
     const router = useRouter()
@@ -18,38 +17,5 @@ export default function RecoverPassword({params}) {
         }, 2000)
     }, [params.token])
 
-    return (
-        <div className="container-fluid d-flex justify-content-center align-items-center h-100 w-100">
-
-            <div className="loader">
-                <div className="box box0">
-                    <div></div>
-                </div>
-                <div className="box box1">
-                    <div></div>
-                </div>
-                <div className="box box2">
-                    <div></div>
-                </div>
-                <div className="box box3">
-                    <div></div>
-                </div>
-                <div className="box box4">
-                    <div></div>
-                </div>
-                <div className="box box5">
-                    <div></div>
-                </div>
-                <div className="box box6">
-                    <div></div>
-                </div>
-                <div className="box box7">
-                    <div></div>
-                </div>
-                <div className="ground">
-                    <div></div>
-                </div>
-            </div>
-        </div>
-    )
+    return <Loading/>
 }
---
+++ source/components/Loading.js
@@ -0,0 +1,37 @@
+import "@/styles/loading.css"
+
+export default function Loading() {
+    return (
+        <div className="container-fluid d-flex justify-content-center align-items-center h-100 w-100">
+            <div className="loader">
+                <div className="box box0">
+                    <div></div>
+                </div>
+                <div className="box box1">
+                    <div></div>
+                </div>
+                <div className="box box2">
+                    <div></div>
+                </div>
+                <div className="box box3">
+                    <div></div>
+                </div>
+                <div className="box box4">
+                    <div></div>
+                </div>
+                <div className="box box5">
+                    <div></div>
+                </div>
+                <div className="box box6">
+                    <div></div>
+                </div>
+                <div className="box box7">
+                    <div></div>
+                </div>
+                <div className="ground">
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
