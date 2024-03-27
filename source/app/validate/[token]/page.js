06459458e3bcb04538797392a855f30a5f1609ee
adriantoral <adrian.rubiboy11@gmail.com>
2024-03-27 12:53:31 +0100

    Agrega la verificacion manual de usuario, ahora el email preview no verificara al usuario automaticamente

--- source/app/validate/[token]/page.js
+++ source/app/validate/[token]/page.js
@@ -1,21 +1,35 @@
 "use client"
 
-import {useEffect} from 'react'
+import {useState} from 'react'
 import {useRouter} from "next/navigation"
-import {validate} from "@/api/v1/auth";
-import Loading from "@/components/Loading";
+import Loading from "@/components/Loading"
+import {validate} from "@/api/v1/auth"
 
 export default function RecoverPassword({params}) {
     const router = useRouter()
+    const [loading, setLoading] = useState(false)
 
-    useEffect(() => {
-        setTimeout(() => {
-            validate(params.token).then((response) => {
-                if (!response) return router.push('/signup')
-                router.push('/signin')
-            })
-        }, 2000)
-    }, [params.token])
+    if (loading) return <Loading/>
 
-    return <Loading/>
+    return (
+        <div className='d-flex flex-column align-items-center justify-content-evenly' style={{minHeight: '60vh'}}>
+            <p className='ms-font fs-5 lead text-center w-50'>
+                Ya estas casi listo para comenzar a usar nuestra plataforma, para validar tu cuenta solo debes hacer click en el siguiente botón.
+            </p>
+
+            <button
+                type='button'
+                disabled={loading}
+                onClick={async () => {
+                    setLoading(true)
+
+                    const data = await validate(params.token)
+                    if (!data) return router.push('/signup')
+                    router.push('/signin')
+                }}
+                className='btn btn-primary border-5 ms-button p-3 w-50' style={{minWidth: "fit-content"}}>
+                VALIDAR CUENTA
+            </button>
+        </div>
+    )
 }
