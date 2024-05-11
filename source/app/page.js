b946541de473fbab8b6204c3a5d076e88b765fe3
adriantoral <adrian.rubiboy11@gmail.com>
2024-05-12 00:45:21 +0200

    Arregla la pagina principal con los cambios de Didi

--- source/app/page.js
+++ source/app/page.js
@@ -12,17 +12,15 @@ export default function HomePage() {
     return (
         <AuthProvider redirect={false}>
             <NavBar/>
-            <div className="container-fluid bg-image-main d-flex flex-column justify-content-center align-items-center p-0">
-                <div className='container text-center' style={{maxWidth: 800}}>
-                    <h1 className="fw-bold display-3">Trabajos de alumnos/is</h1>
-                    <p className="fw-bold  mt-4 fs-5">¡Descubre aquí el talento U-tad!</p>
-                    <p className="pe-1 mt-3 fs-5">Bienvenido al repositorio de proyectos oficial de la U-tad.<br/>
-                        Aquí podrás encontrar todo tipo de trabajos de los alumnos que se han formado aquí!
-                    </p>
+            <div className="container-fluid bg-image-main d-flex flex-column justify-content-center align-items-center text-center p-0">
+                <h1 className="fw-bold display-3">Trabajos de alumnos</h1>
+                <p className="fw-bold  mt-4 fs-5">¡Descubre aquí el talento U-tad!</p>
+                <p className="pe-1 mt-3 fs-5">Bienvenido al repositorio de proyectos oficial de la U-tad.<br/>
+                    Aquí podrás encontrar todo tipo de trabajos de los alumnos que se han formado aquí!
+                </p>
 
-                    <div className="container text-center d-grid gap-2" style={{maxWidth: 512}}>
-                        <button onClick={() => router.push('/home')} className="btn btn-primary mt-5 btn-lg text-center fw-bold fs-3" style={{maxWidth: 512, height: 64}}>ACCEDER</button>
-                    </div>
+                <div className="container text-center d-grid gap-2" style={{maxWidth: '32rem'}}>
+                    <button onClick={() => router.push('/home')} className="btn btn-primary mt-5 btn-lg text-center fw-bold fs-3" style={{maxWidth: 512, height: 64}}>ACCEDER</button>
                 </div>
             </div>
         </AuthProvider>
